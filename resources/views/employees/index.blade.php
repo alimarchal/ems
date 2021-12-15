6ec61c219be23ef938c3b9b26a07969eb7c24b4e
Ishtiaq Awan <ishtiaq.awan@ajk.gov.pk>
2021-12-15 13:38:02 +0500

    employees permisions applied

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -13,6 +13,15 @@
                     <span class="hidden md:inline-block ml-2">Home</span>
                 </a>
 
+                @can('Create Employee')
+                    <a href="{{url('employee/create')}}"  class="flex items-center px-4 py-2 text-gray-600 bg-white border rounded-lg focus:outline-none hover:bg-gray-100 transition-colors duration-200 transform dark:text-gray-200 dark:border-gray-200  dark:hover:bg-gray-700 ml-2" title="Members List">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
+                        </svg>
+                        <span class="hidden md:inline-block ml-2">Add Employee</span>
+                    </a>
+                @endcan
+
                 <a href="javascript:;" id="toggle" class="flex items-center px-4 py-2 text-gray-600 bg-white border rounded-lg focus:outline-none hover:bg-gray-100 transition-colors duration-200 transform dark:text-gray-200 dark:border-gray-200  dark:hover:bg-gray-700 ml-2" title="Members List">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
@@ -114,7 +123,10 @@
                         <th class="py-3 px-6 text-center">Designation</th>
                         <th class="py-3 px-6 text-center">Age</th>
                         <th class="py-3 px-6 text-left">Appointment Date</th>
-                        <th class="py-3 px-6 text-center">Actions</th>
+
+                        @canany(['Update Employee', 'Delete Employee'])
+                            <th class="py-3 px-6 text-center">Actions</th>
+                        @endcanany
                     </tr>
                     </thead>
                     <tbody class="text-black text-sm font-light">
@@ -156,34 +168,38 @@
                             <td class="py-3 px-6 text-center">
                                 {{\Carbon\Carbon::parse($employee->appointment->appointment_date)->format('d-m-Y')}}
                             </td>
-
-                            <td class="py-3 px-6 text-center">
-                                <div class="flex item-center justify-center">
-
-                                    <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                        <a href="{{route('employee.edit', $employee->id)}}">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                 stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                      d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>
-                                            </svg>
-                                        </a>
+                            @canany(['Update Employee', 'Delete Employee'])
+                                <td class="py-3 px-6 text-center">
+                                    <div class="flex item-center justify-center">
+                                        @can('Update Employee')
+                                            <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                <a href="{{route('employee.edit', $employee->id)}}">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                         stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>
+                                                    </svg>
+                                                </a>
+                                            </div>
+                                        @endcan
+                                        @can('Delete Employee')
+                                            <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                <form action="{{route('employee.destroy', $employee->id)}}" method="post" onSubmit="if(!confirm('Are you sure you want to delete?')){return false;}">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <button type="submit" class="w-4">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                             stroke="currentColor">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+                                                        </svg>
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        @endcan
                                     </div>
-                                    <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                        <form action="{{route('employee.destroy', $employee->id)}}" method="post" onSubmit="if(!confirm('Are you sure you want to delete?')){return false;}">
-                                            @csrf
-                                            @method('delete')
-                                            <button type="submit" class="w-4">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                     stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
-                                                </svg>
-                                            </button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </td>
+                                </td>
+                            @endcanany
                         </tr>
                     @endforeach
                     </tbody>
