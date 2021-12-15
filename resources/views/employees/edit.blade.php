f9d3bdafc955d0d55dc23a0b5d6de08cb1780c9f
Ishtiaq Awan <ishtiaq.awan@ajk.gov.pk>
2021-12-15 13:24:23 +0500

    minnor change to edit employee page

--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between items-center">
             <span class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Edit Employees') }}
+                {{ __('Edit Employee') }}
             </span>
 
             <div class="flex justify-center items-center float-right">
