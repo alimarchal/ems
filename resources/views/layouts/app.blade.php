e849a30cd0dafeb70a36a841a8f5aae3631783fe
Ali Raza Marchal <alirazamarchal@hotmail.com>
2021-12-29 13:45:04 +0500

    added select2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,11 @@
         <script src="{{ mix('js/app.js') }}" defer></script>
         @if(Route::is('leave.*') )
             <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet"/>
+            <style>
+                .select2 {
+                    width:100%!important;
+                }
+            </style>
         @else
             <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
         @endif
